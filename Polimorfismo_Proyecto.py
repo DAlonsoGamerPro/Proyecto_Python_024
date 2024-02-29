720ee3876ecb1f984ad16672731e623baad6efee
DAlonsoGamerPro <109617599+DAlonsoGamerPro@users.noreply.github.com>
2024-02-29 10:56:09 -0600

    Add files via upload

---
+++ Polimorfismo_Proyecto.py
@@ -0,0 +1,55 @@
+class Minecraft():
+    def Resumen(self):
+        print("\n","Resumen de Minecraft","\n")
+        print("Minecraft es un juego que salió oficielmente en 2011, pero a tenido muchas versiones beta, alpha y test(tan básicas como un campo abierto) abiertas al público, actuelmente la version más nueva es la 1.21 ya que hoy en día sacan una por año.")
+    
+    def Mecanicas(self):
+        print("\n","Mecánicas generales de Minecraft","\n")
+        print("Minecraft es un juego tipo sandbox, es decir caja de arena, ya que puedes hacer prácticamente todo, puedes jugar un modo por defecto, un supervivencia normal, un modo construcción, una version más difícil del supervivencia porque tienes 1 vida, u otras creadas por fans en servidores, como juegos del hambre, TNT Run, o hasta programar con los bloques de comandos o desde el código del juego para hacer mods, de los cuales hay un montón, desde mejoras en el terreno hasta otras dimensiones.")
+        
+class Roblox():
+    def Resumen(self):
+        print("\n","Resumen de Roblox","\n")
+        print("Roblox es un juego del 2009 que en sí no es un juego, es un motor para que otros puedan hacer sus propios juegos, la inmensa mayoría entre malos y mediocres ya que son hechos por niños sin mucha experiencia o estándares, es decir, uno de los mejores juegos de Roblox es una copia de un juego de celular, y otro es abrir 100 puertas mientras que a veces te gritan en la cara.")
+    
+    def Mecanicas(self):
+        print("\n","Mecánicas generales de Roblox","\n")
+        print("Como anteriormente dije, Roblox es más que un juego, un motor para crear experiencias propias, así que hecharé veneno de la empresa, tengo 13 años puedo hacerlo, es un poco perturbador que su negocio se base en tener niños trabajando gratis para ellos, además que los estándares de calidad están por los suelos, desde copias de juegos famosos como COD y Poppy playtime(jaja) hasta juegos del propio Roblox, te pregunto ¿Cuántas copias de DOORS crees que hayan en Roblox? y no juegos 'basados' en DOORS, si no copias como tal, en fin, tiene muchas cosas malas, pero algo bueno que tiene es que como es para todas las edades un papá puede jugarlo sin problemas con sus hijos, eso es todo lo bueno ¿Qué esperas de una empresa que hasta robó el sonido de muerte? si, el 'Oufff' de cuando mueres era robado.")
+        
+class Five_Nights_at_Freddys_Help_Wanted_2():
+    def Resumen(self):
+        print("\n","Resumen de FNaF HP2","\n")
+        print("Si mucho texto, pero bueno, Five Nights at Freddys: Help Wanted 2 o FNaF:HP2 que le dire así por tu bien y el mío, es el juego más reciente de la franquisia de Five Nights at Freddys desarrollado por Steel Wool Studios, es un juego en RV que tiene varios minijuegos y dos finales encontrados.")
+        
+    def Mecanicas(self):
+        print("\n","Mecánicas de FNaF:HP2","\n")
+        print("Este juego tiene varios minijuego como ya dije, hay de servirle comida a los Staff bots para entrenarlos, relacionados con FNaF Sister Location, de reparar robots, el mejor de todos, uno de ser médico y ayudar a Helpie, un pequeño freddy blanco y rosa, uno de disparos y otros.")
+        
+
+minecraft = Minecraft()
+minecraft.Resumen()
+minecraft.Mecanicas()
+
+roblox = Roblox()
+roblox.Resumen()
+roblox.Mecanicas()
+
+freddys = Five_Nights_at_Freddys_Help_Wanted_2()
+freddys.Resumen()
+freddys.Mecanicas()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
